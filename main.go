b763269f8bf80521d3abb5069500c5ab9e26399c
666savages <egor0263s@gmail.com>
2021-10-19 13:21:14 +0500

    1910212

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
+	"fmt"
 	"net"
 	"os"
 	"strings"
@@ -34,7 +36,7 @@ func init() {
 }
 
 func main() {
-	node := NewNode(os.Args[1]).Run(handleServer, handleClient)
+	NewNode(os.Args[1]).Run(handleServer, handleClient)
 }
 
 //ipv4:port
@@ -59,7 +61,45 @@ func (node *Node) Run(handleServer func(*Node), handleClient func(*Node)) {
 
 //сервер на принятие данных с определенного порта
 func handleServer(node *Node) {
-	listen, tcp := net.Listen("tcp", "0.0.0.0"+node.Address.Port)
+	listen, err := net.Listen("tcp", "0.0.0.0"+node.Address.Port)
+	if err != nil {
+		panic("listen error")
+	}
+	defer listen.Close()
+
+	for {
+		//приятие соединения
+		conn, err := listen.Accept()
+		if err != nil {
+			break
+		}
+		go handleConnection(node, conn)
+	}
+}
+
+func handleConnection(node *Node, conn net.Conn) {
+	defer conn.Close()
+	var (
+		buffer  = make([]byte, 512)
+		message string
+		pack    Package
+	)
+	for {
+		length, err := conn.Read(buffer)
+		if err != nil {
+			break
+		}
+		message += string(buffer[:length])
+	}
+	//расшифровка JSON файла
+	err := json.Unmarshal([]byte(message), &pack)
+	if err != nil {
+		return
+	}
+	//проанализировать от кого отправлен пакет
+	node.ConnectTo([]string{pack.From})
+	//смотрим что отправил пользователь
+	fmt.Println(pack.Data)
 }
 
 func handleClient(node *Node) {
@@ -71,9 +111,18 @@ func handleClient(node *Node) {
 			os.Exit(0)
 		case "/connect":
 			node.ConnectTo(splited[1:])
+		case "/network":
+			node.PrintNetWork()
 		default:
 			node.SendMessageToAll(message)
 		}
+	}
+}
+
+func (node *Node) PrintNetWork() {
+	//перебор адресов в соединениях
+	for addr := range node.Connections {
+		fmt.Println("|", addr)
 	}
 }
 
@@ -85,13 +134,13 @@ func (node *Node) ConnectTo(addresses []string) {
 }
 
 func (node *Node) SendMessageToAll(message string) {
-	var new_pack = &Package{
+	var new_pack = Package{
 		From: node.Address.IPv4 + node.Address.Port,
 		Data: message,
 	}
 	for addr := range node.Connections {
 		new_pack.To = addr
-		node.Send(new_pack)
+		node.Send(&new_pack)
 	}
 }
 
@@ -104,4 +153,9 @@ func (node *Node) Send(pack *Package) {
 	defer conn.Close()                  //закрытие соединения
 	json_pack, _ := json.Marshal(*pack) //перевод в JSON формат
 	conn.Write(json_pack)
+}
+
+func InputString() string {
+	msg, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	return strings.Replace(msg, "\n", "", -1)
 }
